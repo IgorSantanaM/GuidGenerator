75c943d1dad68df56e8f22bfdd2623255b01ceb2
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-07-22 22:05:15 -0300

    Initial commit

---
+++ GuidGenerator/Generator.cs
@@ -0,0 +1,14 @@
+﻿namespace GuidGenerator
+{
+    public static class Generator
+    {
+        public static void GuidGenerate(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Guid guid = Guid.NewGuid();
+                Console.WriteLine(guid);
+            }
+        }
+    }
+}
---
+++ GuidGenerator/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace GuidGenerator;
+class Program
+{
+    public static void Main()
+    {
+        Generator.GuidGenerate(250);
+    }
+}

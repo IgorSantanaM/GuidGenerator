ca5453e092140b747b3a79b0ba284134dc6d5276
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-08-07 16:11:32 -0400

    (feat): Made it append the text to a path

--- GuidGenerator/Generator.cs
+++ GuidGenerator/Generator.cs
@@ -1,13 +1,21 @@
-﻿namespace GuidGenerator
+﻿using System;
+using System.IO;
+
+namespace GuidGenerator
 {
     public static class Generator
     {
         public static void GuidGenerate(int times)
         {
+            string filePath = "X:\your\path\here.txt";
+
             for (int i = 0; i < times; i++)
             {
                 Guid guid = Guid.NewGuid();
-                Console.WriteLine(guid);
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.WriteLine(guid);
+                }
             }
         }
     }
